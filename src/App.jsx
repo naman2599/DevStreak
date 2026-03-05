8efd300cd9f913bf59de865d4101f3dfcd1b0b97
naman2599 <33091749+naman2599@users.noreply.github.com>
2026-03-05 23:55:39 +0530

    Create App.jsx with a React component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to DevStreak!</h1>
+            <p>Keep track of your development milestones.</p>
+        </div>
+    );
+};
+
+export default App;
